722fc33c2b4928b882b03746bc763a90a64e8309
ienw <ienw@metropolia.fi>
2021-03-18 13:34:28 +0200

    pug exercise

---
+++ week1/server-two/index.js
@@ -0,0 +1,24 @@
+"use strict";
+const express = require("express");
+const app = express();
+const port = 3000;
+const pug = require('pug');
+
+app.use(express.static('public'));
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+app.get("/catinfo", (req, res) => {
+  const cat = { name: "Frank", age: 6, weight: 5 };
+  res.json(cat);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
