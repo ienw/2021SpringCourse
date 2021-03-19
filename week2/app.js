12e8ee71769b3240307e77e3a8f815ac7961cd48
ienw <ienw@metropolia.fi>
2021-03-19 12:57:43 +0200

    updates for rest of exerciese

---
+++ week2/app.js
@@ -0,0 +1,22 @@
+'use strict';
+const express = require('express');
+const catRoute = require('./routes/catRoute');
+const userRoute = require('./routes/userRoute');
+const authRoute = require('./routes/authRoute');
+const passport = require('./utils/pass');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use(express.json()); //for parsing application/json
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static('week2_public_html'));
+
+app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
+app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
+app.use('/auth', authRoute);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
